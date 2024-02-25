4106341b6577f91d8be79a5ee18db432d4e05a33
Çağdaş Taş <143749465+tascagdas@users.noreply.github.com>
2024-02-25 14:22:30 +0300

    ??

--- UlukunShopAPI/Infrastructure/UlukunShopAPI.Persistence/Concretes/ProductService.cs
+++ UlukunShopAPI/Infrastructure/UlukunShopAPI.Persistence/Concretes/ProductService.cs
@@ -9,9 +9,9 @@ public class ProductService:IProductService
         => new()
         {
             new(){Id = Guid.NewGuid(),Name = "Product 1",Price = 100,Stock = 10},
-            new(){Id = Guid.NewGuid(),Name = "Product 2",Price = 200,Stock = 10},
-            new(){Id = Guid.NewGuid(),Name = "Product 3",Price = 300,Stock = 10},
-            new(){Id = Guid.NewGuid(),Name = "Product 4",Price = 400,Stock = 10},
-            new(){Id = Guid.NewGuid(),Name = "Product 5",Price = 500,Stock = 10}
+            new(){Id = Guid.NewGuid(),Name = "Product 2",Price = 200,Stock = 8},
+            new(){Id = Guid.NewGuid(),Name = "Product 3",Price = 300,Stock = 7},
+            new(){Id = Guid.NewGuid(),Name = "Product 4",Price = 400,Stock = 5},
+            new(){Id = Guid.NewGuid(),Name = "Product 5",Price = 500,Stock = 3}
         };
 }
